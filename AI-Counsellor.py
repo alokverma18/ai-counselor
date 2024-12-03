16534085bc77d40c7c852503e83b0f1025db321f
Alok Verma <as1492@srmist.edu.in>
2024-12-03 13:40:20 +0000

    Gemini!

--- AI-Counsellor.py
+++ AI-Counsellor.py
@@ -1,19 +1,23 @@
 import streamlit as st
-import google.generativeai as palm
+import google.generativeai as genai
 
+api_key = st.secrets["api_key"]
 # Configure the generative AI model and API key
-palm.configure(api_key='AIzaSyBZZBj_c1TDDWAYgDg8GUqGiJYX4uzSjxY')
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
-model = models[0].name
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 st.title("Student Career Counselor Chatbot")
 
 # Initial chatbot response
-response = palm.chat(context="Act like a Student Career Counsellor.", messages='Hello')
+response = model.generate_content("Act as a Student Career Counselor. Hello!")
 st.write("Hello, I'm your Student Career Counselor, how can I help you?")
 
-user_message = st.text_input("Enter your message:")
+user_message = st.text_area("Enter your message:")
+submit_button = st.button("Submit")
 
-if user_message:
-    response = response.reply(user_message)
-    st.write("Bot:", response.last)
+if submit_button:
+    if user_message:
+        prompt = f"You: {user_message}\nCounselor:"
+        response = model.generate_content(prompt)
+        st.write("Bot:", response.text)
+        user_message = ""  # Clear the text area after submission
